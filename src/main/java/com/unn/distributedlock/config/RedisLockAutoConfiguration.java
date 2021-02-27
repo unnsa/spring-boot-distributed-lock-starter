466b3bb235cf177b61406b75448934d498ca80fb
yangj <yangjiyun12@hotmail.com>
2021-02-27 12:50:39 +0800

    分布式锁

--- src/main/java/com/unn/distributedlock/config/RedisLockAutoConfiguration.java
+++ src/main/java/com/unn/distributedlock/config/RedisLockAutoConfiguration.java
@@ -2,9 +2,12 @@ package com.unn.distributedlock.config;
 
 import com.unn.distributedlock.aspect.RedisLockAspect;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.integration.support.locks.ExpirableLockRegistry;
 
 /**
  * redis lock auto configuration
@@ -12,6 +15,7 @@ import org.springframework.context.annotation.Import;
  * @author yangjiyun
  */
 @Configuration
+@ConditionalOnClass({StringRedisTemplate.class, ExpirableLockRegistry.class,})
 @AutoConfigureAfter(RedisAutoConfiguration.class)
 @Import({RedisLockAspect.class})
 public class RedisLockAutoConfiguration {
