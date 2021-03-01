9139baa2e1984d92b1d9f113b9dbf5381b0dea90
yangjiyun <yangjiyun12@hotmail.com>
2021-03-01 17:29:06 +0800

    mix bug

--- src/main/java/com/unn/distributedlock/aspect/RedisLockAspect.java
+++ src/main/java/com/unn/distributedlock/aspect/RedisLockAspect.java
@@ -2,17 +2,12 @@ package com.unn.distributedlock.aspect;
 
 import com.unn.distributedlock.annotation.DistributedLock;
 import com.unn.distributedlock.core.RedisLockRegistry;
-import com.unn.distributedlock.event.AcquireLockExceptionEvent;
-import com.unn.distributedlock.event.AcquireLockFailedEvent;
-import com.unn.distributedlock.event.AfterUnlockSuccessEvent;
-import com.unn.distributedlock.event.BeforeLockEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import lombok.var;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
-import org.springframework.context.ApplicationContext;
 import org.springframework.core.annotation.Order;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.stereotype.Component;
@@ -36,7 +31,6 @@ import java.util.function.Function;
 public class RedisLockAspect {
     private final RedisConnectionFactory redisConnectionFactory;
     private final Map<String, RedisLockRegistry> redisLockRegistryMap = new ConcurrentHashMap<>();
-    private final ApplicationContext applicationContext;
 
 
     @Around(value = "@annotation(distributedLock)")
@@ -47,9 +41,6 @@ public class RedisLockAspect {
                 .filter(r -> r);
         if (!lockedOp.isPresent()) {
             log.info("Failed to lock mutex at " + distributedLock.key());
-            applicationContext.publishEvent(AcquireLockFailedEvent.builder()
-                    .args(joinPoint.getArgs())
-                    .build());
             return null;
         }
         log.debug("------加锁成功,开始处理业务逻辑------");
@@ -61,27 +52,14 @@ public class RedisLockAspect {
 
     @Before(value = "@annotation(distributedLock)")
     public void before(JoinPoint joinPoint, DistributedLock distributedLock) {
-        applicationContext.publishEvent(BeforeLockEvent.builder()
-                .args(joinPoint.getArgs())
-                .distributedLock(distributedLock)
-                .build());
     }
 
     @AfterReturning(value = "@annotation(distributedLock)")
     public void afterReturning(JoinPoint joinPoint, DistributedLock distributedLock) {
-        applicationContext.publishEvent(AfterUnlockSuccessEvent.builder()
-                .args(joinPoint.getArgs())
-                .distributedLock(distributedLock)
-                .build());
     }
 
     @AfterThrowing(value = "@annotation(distributedLock)", throwing = "ex")
     public void afterThrowing(JoinPoint joinPoint, DistributedLock distributedLock, Throwable ex) {
-        applicationContext.publishEvent(AcquireLockExceptionEvent.builder()
-                .args(joinPoint.getArgs())
-                .distributedLock(distributedLock)
-                .e(ex)
-                .build());
     }
 
 
