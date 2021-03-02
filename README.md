84c454c379511abf6cd385b270798096c62218f6
yangjiyun <yangjiyun12@hotmail.com>
2021-03-02 16:43:21 +0800

    增加简单说明

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 ## 分布式锁
-1. 使用方式
+1. 支持重入
 
-   使用@DistributedLock，在方法上标注，表示进入该方法需要获取redis分布式锁，详情见注解中的注释
+2. 支持自动续租.
+   当到达锁的过期时间`expireAfter`的2/3时,如果还未解锁,则自动续租,将锁的过期时间重置为`expireAfter`
+   
+3. 使用lua脚本, 操作redis具有原子性
+
+4. 例子:
+
+   ```java
+    @Autowired
+   private RedisLockRegistryUtil redisLockRegistryUtil;
+   public void test2() throws InterruptedException {
+           Lock lock = redisLockRegistryUtil.getLockAutoKeepLease("lockName", 10, TimeUnit.SECONDS, "lockKey");
+           if (!lock.tryLock(10,TimeUnit.SECONDS)){
+               //do something
+               return;
+           }
+           lock.unlock();
+       }
+   ```
+
+   
+
--- src/test/java/com.unn.distributedlock/RedisLockTest.java
+++ src/test/java/com.unn.distributedlock/RedisLockTest.java
@@ -1,16 +1,22 @@
 package com.unn.distributedlock;
 
+import com.unn.distributedlock.util.RedisLockRegistryUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+
 //@RunWith(SpringRunner.class)
 //@SpringBootTest(classes = RedisLockTestApplication.class)
 public class RedisLockTest {
     @Autowired
     private BusinessService businessService;
+    @Autowired
+    private RedisLockRegistryUtil redisLockRegistryUtil;
 
 
     //@Test
@@ -20,5 +26,15 @@ public class RedisLockTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    //@Test
+    public void test2() throws InterruptedException {
+        Lock lock = redisLockRegistryUtil.getLockAutoKeepLease("lockName", 10, TimeUnit.SECONDS, "lockKey");
+        if (!lock.tryLock(10,TimeUnit.SECONDS)){
+            //do something
+            return;
+        }
+        lock.unlock();
     }
 }
